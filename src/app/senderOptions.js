24e8e01d818857015cc4073929c2ca079eba86be
SergWebAcceptance <serg.web@acceptance.ltd>
2025-07-28 16:58:25 +0300

    Added tanzora.io as a sender option in senderOptions.js

--- src/app/senderOptions.js
+++ src/app/senderOptions.js
@@ -12,4 +12,5 @@ export const senderOptions = [
   { value: "info@grimbix.io", label: "info@grimbix.io" },
   { value: "info@mercantix.io", label: "info@mercantix.io" },
   { value: "info@metriccode.io", label: "info@metriccode.io" },
+  { value: "info@tanzora.io", label: "info@tanzora.io" },
 ];
